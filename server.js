a87c498d2ed297288398fa69f9f06b172819c030
MateusGSantos99 <1067790@senacminas.edu.br>
2025-08-07 16:35:56 -0300

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors({
-  origin: 'https://frontend-portal-turismo-zeta.vercel.app/',
+  origin: 'https://frontend-portal-turismo-zeta.vercel.app',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
 }));
